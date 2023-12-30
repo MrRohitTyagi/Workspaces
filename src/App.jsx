125ebce6b35e68b87c328dfd75d6e3f53b5baec9
Rohit <rohit.tyagi@thoughts2binary.com>
2023-12-30 08:03:06 +0530

    8

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import Authorize from "./components/authorizeUser";
 import { Route, Routes } from "react-router-dom";
 import LoginScreen from "./components/loginScreen";
 import { ToastContainer } from "react-toastify";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
+const socket = io("http://localhost:4000", {
+  transports: ["websocket", "polling", "flashsocket"],
+});
 function App() {
+  useEffect(() => {
+    // Listen for messages from the server
+    socket.on("CONNECTED", (id) => {
+      window.socket_id = id;
+    });
+    socket.on("NEW_EMAIL_RECEIVED", (email) => {
+      console.log(
+        `%c NEW EMAIL RECEIVED `,
+        "color: yellow;border:1px solid lightgreen",
+        email
+      );
+    });
+    // Clean up the socket connection on component unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <>
       <Routes>
@@ -25,5 +47,4 @@ function App() {
     </>
   );
 }
-
 export default App;
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -4,13 +4,29 @@ const baseUrl = import.meta.env.VITE_BE_BASE_URL;
 export async function caller(type, extendedUrl, body) {
   switch (type) {
     case "get":
-      return await axios.get(baseUrl + extendedUrl, body);
+      return await axios.get(baseUrl + extendedUrl, body, {
+        headers: {
+          socket_id: window.socket_id,
+        },
+      });
     case "delete":
-      return await axios.delete(baseUrl + extendedUrl, body);
+      return await axios.delete(baseUrl + extendedUrl, body, {
+        headers: {
+          socket_id: window.socket_id,
+        },
+      });
     case "post":
-      return await axios.post(baseUrl + extendedUrl, body);
+      return await axios.post(baseUrl + extendedUrl, body, {
+        headers: {
+          socket_id: window.socket_id,
+        },
+      });
     case "put":
-      return await axios.put(baseUrl + extendedUrl, body);
+      return await axios.put(baseUrl + extendedUrl, body, {
+        headers: {
+          socket_id: window.socket_id,
+        },
+      });
     default:
       break;
   }
